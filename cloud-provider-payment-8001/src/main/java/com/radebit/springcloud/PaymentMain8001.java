270144e746006dad502a7f80f8f6a5245d2c1171
radebit <26564356@qq.com>
2020-07-04 13:28:21 +0800

    8001加入eureka客户端

--- cloud-provider-payment-8001/src/main/java/com/radebit/springcloud/PaymentMain8001.java
+++ cloud-provider-payment-8001/src/main/java/com/radebit/springcloud/PaymentMain8001.java
@@ -3,6 +3,7 @@ package com.radebit.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Rade
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan({"com.radebit.springcloud.mapper*"})
+@EnableEurekaClient // 应用Eureka客户端
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
