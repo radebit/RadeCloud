565bab9aa7107d92f93b2ff1ecdef18f7de7cdcb
radebit <26564356@qq.com>
2020-07-05 14:05:16 +0800

    配置Eureka集群

---
+++ cloud-eureka-server-7002/src/main/java/com/radebit/springcloud/EurekaMain7002.java
@@ -0,0 +1,19 @@
+package com.radebit.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Rade
+ * @date 2020/7/5 1:45 下午
+ * @url https://blog.radebit.com
+ * 说明：
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
