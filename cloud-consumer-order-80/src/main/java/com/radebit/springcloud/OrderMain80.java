ca6ee66350cd4668214daa074c0792967a0edfc0
radebit <26564356@qq.com>
2020-07-05 13:28:32 +0800

    80加入eureka客户端

--- cloud-consumer-order-80/src/main/java/com/radebit/springcloud/OrderMain80.java
+++ cloud-consumer-order-80/src/main/java/com/radebit/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.radebit.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Rade
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 说明：
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
