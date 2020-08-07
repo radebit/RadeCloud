0dd6405918e7759da72a967eb3f5413f5730eb0e
radebit <26564356@qq.com>
2020-08-07 15:45:11 +0800

    新建Hystrix服务

---
+++ cloud-provider-hystrix-payment-8006/src/main/java/com/radebit/springcloud/PaymentHystrixMain8006.java
@@ -0,0 +1,19 @@
+package com.radebit.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Rade
+ * @date 2020/8/7 3:37 下午
+ * @url https://blog.radebit.com
+ * 说明：
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentHystrixMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8006.class, args);
+    }
+}
