eb668384fdfbd1d0d6f499ef014b45554ba255b4
radebit <26564356@qq.com>
2020-09-07 12:47:52 +0800

    服务降级 超时设置

--- cloud-provider-hystrix-payment-8006/src/main/java/com/radebit/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment-8006/src/main/java/com/radebit/springcloud/service/impl/PaymentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.radebit.springcloud.service.impl;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.radebit.springcloud.service.PaymentService;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,7 @@ public class PaymentServiceImpl implements PaymentService {
      */
     @Override
     public String paymentInfoOk(Integer id) {
-        return "线程池：" + Thread.currentThread().getName() + " paymentInfo_ok,id：" + id + "\t SUCCESS";
+        return "线程池：" + Thread.currentThread().getName() + " paymentInfoOk,id：" + id + "\t SUCCESS";
     }
 
     /**
@@ -27,14 +28,27 @@ public class PaymentServiceImpl implements PaymentService {
      * @return
      */
     @Override
-    @HystrixCommand(fallbackMethod = "paymentInfoTimeoutHandler")   // 出问题后兜底的方法
+    @HystrixCommand(fallbackMethod = "paymentInfoTimeoutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+    })   // 出问题后兜底的方法
     public String paymentInfoTimeout(Integer id) {
         int timeNum = 3;
+//        int errorTest = 10 / 0;
         try {
             TimeUnit.SECONDS.sleep(timeNum);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return "线程池：" + Thread.currentThread().getName() + " paymentInfo_timeout,id：" + id + "\t TIMEOUT " + " 耗时：" + timeNum + "秒";
+        return "线程池：" + Thread.currentThread().getName() + " paymentInfoTimeout,id：" + id + "\t TIMEOUT " + " 耗时：" + timeNum + "秒";
+    }
+
+    /**
+     * paymentInfoTimeout的fallback方法
+     *
+     * @param id
+     * @return
+     */
+    public String paymentInfoTimeoutHandler(Integer id) {
+        return "线程池：" + Thread.currentThread().getName() + " paymentInfoTimeoutHandler,id：" + id + "\t 访问出错了，请稍后再试！";
     }
 }
