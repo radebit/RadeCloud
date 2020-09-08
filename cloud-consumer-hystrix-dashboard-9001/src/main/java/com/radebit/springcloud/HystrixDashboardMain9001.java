31427012260d961a3260210fc88aa7be8c342b85
radebit <26564356@qq.com>
2020-09-08 16:09:23 +0800

    新增Hystrix Dashboard监控

---
+++ cloud-consumer-hystrix-dashboard-9001/src/main/java/com/radebit/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.radebit.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author Rade
+ * @date 2020/9/8 4:01 下午
+ * @url https://blog.radebit.com
+ * 说明：
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
