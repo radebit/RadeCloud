23a77cb0e0ddf42976564d01b08a9dd478f3a164
radebit <26564356@qq.com>
2020-09-10 22:03:13 +0800

    新增GateWay网关

---
+++ cloud-gateway-9527/src/main/java/com/radebit/springcloud/GateWayMain9527.java
@@ -0,0 +1,19 @@
+package com.radebit.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Rade
+ * @date 2020/9/10 1:58 下午
+ * @url https://blog.radebit.com
+ * 说明：
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
