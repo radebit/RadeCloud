32024ae09fbac25e19ff537f9174f4fc193034f0
Rade <26564356@qq.com>
2021-05-14 15:45:32 +0800

    新增一个Stream消费者

---
+++ cloud-stream-rabbitmq-consumer-8803/src/main/java/springcloud/StreamMQMain8803.java
@@ -0,0 +1,18 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author Rade
+ * @Date 2021/5/14 14:58:58
+ * @Description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
