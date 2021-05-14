b0d46af2d7166b3289a04507d327e727eb7dc9bc
Rade <26564356@qq.com>
2021-05-14 23:08:48 +0800

    新增83消费端并测试nacos负载均衡

---
+++ cloud-alibaba-consumer-order-83/src/main/java/com/radebit/springcloud/OrderNacosMain83.java
@@ -0,0 +1,18 @@
+package com.radebit.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author Rade
+ * @Date 2021/5/14 22:58:58
+ * @Description
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OrderNacosMain83 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class, args);
+    }
+}
---
+++ cloud-alibaba-consumer-order-83/src/main/java/com/radebit/springcloud/controller/OrderNacosController.java
@@ -0,0 +1,30 @@
+package com.radebit.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author Rade
+ * @Date 2021/5/14 23:00:00
+ * @Description
+ */
+@RestController
+@RequestMapping("/consumer")
+public class OrderNacosController {
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${service-url.nacos-user-service}")
+    private String serverURL;
+
+    @GetMapping("/payment/nacos/{id}")
+    public String paymentInfo(@PathVariable("id") Integer id) {
+        return restTemplate.getForObject(serverURL + "/payment/nacos/" + id, String.class);
+    }
+}
