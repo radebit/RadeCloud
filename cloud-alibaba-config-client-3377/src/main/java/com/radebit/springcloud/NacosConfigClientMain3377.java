05ef1f475b607ac6b5cd8713f97df615dc098969
Rade <26564356@qq.com>
2021-05-16 21:27:49 +0800

    新增Nacos服务配置Config客户端

---
+++ cloud-alibaba-config-client-3377/src/main/java/com/radebit/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.radebit.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author Rade
+ * @Date 2021/5/16 20:49:49
+ * @Description
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
