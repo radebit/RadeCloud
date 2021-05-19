2571fc964a57643f2a774365ae2632f9c93690bf
Rade <26564356@qq.com>
2021-05-19 20:56:48 +0800

    热点规则测试

--- cloud-alibaba-sentinel-service-8401/src/main/java/com/radebit/springcloud/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service-8401/src/main/java/com/radebit/springcloud/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.radebit.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,5 +26,16 @@ public class FlowLimitController {
     @GetMapping("/testC")
     public String testC() {
         return "===== TestC =====";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "testHotKey p1=" + p1 + ",p2=" + p2;
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException blockException) {
+        return "testHotKey出现异常";
     }
 }
