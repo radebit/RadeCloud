719d83e267cb2c1014951e44f9ba1063113f003d
Rade <26564356@qq.com>
2021-05-20 23:23:56 +0800

    Sentinel整合Ribbon+OpenFeign+Fallback

--- cloud-alibaba-consumer-order-84/src/main/java/com/radebit/springcloud/controller/CircleBreakerController.java
+++ cloud-alibaba-consumer-order-84/src/main/java/com/radebit/springcloud/controller/CircleBreakerController.java
@@ -1,6 +1,7 @@
 package com.radebit.springcloud.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.radebit.springcloud.entities.CommonResult;
 import com.radebit.springcloud.entities.Payment;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,7 +27,14 @@ public class CircleBreakerController {
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/fallback/{id}")
-    @SentinelResource(value = "fallback")
+//    @SentinelResource(value = "fallback")   // 没有任何配置的情况
+//    @SentinelResource(value = "fallback", fallback = "handlerFallback")  // fallback只负责处理业务异常
+//    @SentinelResource(value = "fallback", blockHandler = "blockHandler")  // blockHandler只负责处理限流
+//    @SentinelResource(value = "fallback", fallback = "handlerFallback", blockHandler = "blockHandler")  // 都配置
+    @SentinelResource(value = "fallback",
+            fallback = "handlerFallback",
+            blockHandler = "blockHandler",
+            exceptionsToIgnore = {IllegalArgumentException.class})  // 排除
     public CommonResult<Payment> fallback(@PathVariable("id") Long id) {
         if (id == 4) {
             throw new IllegalArgumentException("IllegalArgumentException，非法参数异常！");
@@ -36,5 +44,17 @@ public class CircleBreakerController {
             throw new NullPointerException("NullPointerException，该ID没有对应记录，空指针异常！");
         }
         return result;
+    }
+
+    // handlerFallback
+    public CommonResult<Payment> handlerFallback(@PathVariable("id") Long id, Throwable throwable) {
+        Payment payment = new Payment(id, "null");
+        return new CommonResult<>(503, "fallback异常兜底，exception：" + throwable.getMessage(), payment);
+    }
+
+    // blockHandler
+    public CommonResult<Payment> blockHandler(@PathVariable("id") Long id, BlockException blockException) {
+        Payment payment = new Payment(id, "null");
+        return new CommonResult<>(500, "blockHandler-Sentinel限流，blockException：" + blockException.getMessage(), payment);
     }
 }
